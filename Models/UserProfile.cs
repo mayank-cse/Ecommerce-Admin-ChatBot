bc9e5d65133c65f89085272f61024fa43c39050a
mayank-cse <72187020+mayank-cse@users.noreply.github.com>
2022-06-28 21:43:01 +0530

    Ecommerce Admin Chatbot

---
+++ Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceAdminBot.Models
+{
+    public class UserProfile
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool UserAuthenticated { get; set; } = false;
+        public int OTP { get; set; }
+        public string ValueFinder { get; set; }
+    }
+}
